54e6b791a67747c315b1f05903cae17ee6dad1f0
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-03-29 11:43:13 +0100

    Class representing philosopher semaphore created.

---
+++ project1/philosopher.h
@@ -0,0 +1,16 @@
+#define THINKING 0
+#define EATING 1
+#define WAITING 2
+
+#include <iostream>
+#include <stdlib.h>  // for strtol
+using namespace std;
+
+
+// Philosopher semaphore 
+class Philosopher
+{
+    int state;
+
+};
+
--- project1/source_code.cpp
+++ project1/source_code.cpp
@@ -1,15 +1,26 @@
-#include <iostream>
-#include <stdlib.h>  // for strtol
-
-using namespace std;
+#include "philosopher.h"
 
 int N = 0; // Amount of philosophers
 
+
+Philosopher *philosophers;
 
 int main(int argc, char *argv[]) {
 	if(argc>1){
 		N=strtol(argv[1], argv, 10);
 	}
+    else {
+        cout << "No argument given.";
+        return -1;
+    }
+
+    philosophers = (Philosopher*)malloc(N * sizeof(Philosopher));
+
+    if(philosophers==NULL){
+        cout << "Allocating memory for philosophers failed.";
+        return -1;
+    }
+
 
 	return 0;
 }
